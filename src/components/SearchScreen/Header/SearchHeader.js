fc36c90f53c11e86ff463343a94bb3d7affbdecc
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2023-02-19 13:30:00 +0530

    Search Sub Header Completed

--- src/components/SearchScreen/Header/SearchHeader.js
+++ src/components/SearchScreen/Header/SearchHeader.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { FaMicrophone, FaSearch } from "react-icons/fa";
 import { CgMenuGridO } from "react-icons/cg";
 import '../../Styles/SearchHeader.css';
 
 const SearchHeader = ({ searchTerm }) => {
+
+    const [searchText, setSearchText] = useState('');
+    useEffect(() => {
+        setSearchText(searchTerm)
+    }, [searchTerm])
+
     const nav = useNavigate();
     return (
         <div className='search_header'>
@@ -12,12 +18,13 @@ const SearchHeader = ({ searchTerm }) => {
                 <img src='https://www.google.co.in/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png' alt='Google Logo' />
             </Link>
             <form className='search_header-input'>
-                <input type="text" value={searchTerm} />
+                <input type="text" value={searchText} />
                 <div className='search_header-icons'>
-                    <FaMicrophone />
-                    <FaSearch />
+                    <FaMicrophone style={{cursor:'pointer'}}/>
+                    <FaSearch style={{cursor:'pointer'}}/>
                 </div>
             </form>
+            <CgMenuGridO />
         </div>
     )
 }
--- src/components/SearchScreen/Header/SearchSubHeader.js
+++ src/components/SearchScreen/Header/SearchSubHeader.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import { FaSearch, FaImage, FaVideo, FaMapMarkerAlt, FaRegNewspaper, FaBars  } from "react-icons/fa";
+import '../../Styles/SearchSubHeader.css';
 
 const SearchSubHeader = () => {
   return (
-    <div>SearchSubHeader</div>
+    <div className='search_Sub_header'>
+      <ul>
+        <li>
+          <Link to={'/'}><FaSearch className='search_Sub_header-icon'/> All</Link>
+        </li>
+        <li>
+          <Link to={'/'}><FaRegNewspaper className='search_Sub_header-icon'/> News</Link>
+        </li>
+        <li>
+          <Link to={'/'}><FaMapMarkerAlt className='search_Sub_header-icon'/> Maps</Link>
+        </li>
+        <li>
+          <Link to={'/'}><FaImage className='search_Sub_header-icon'/> Images</Link>
+        </li>
+        <li>
+          <Link to={'/'}><FaVideo className='search_Sub_header-icon'/> Videos</Link>
+        </li>
+        <li>
+          <Link to={'/'}><FaBars className='search_Sub_header-icon'/> More</Link>
+        </li>
+      </ul>
+    </div>
   )
 }
 
